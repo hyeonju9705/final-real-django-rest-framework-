108f48215cfd4c86025f4fc5f0debc7269cc0cee
heyonju <hyeonju9705@likelion.org>
2019-12-30 17:18:48 +0900

    커밋 코멘트

---
+++ backend/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\pc\desktop\멋사과제\마지막 세션\마지막세션 ㅠㅠ.txt\backend\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
